3ed20d1b8678947cd8737be5a5f7e568be2381eb
Sacha Roland Robert Godey <godey@izar1.izar.cluster>
2025-05-11 19:28:12 +0200

    new label for training + creation of a test csv file

--- dataset_module/notebooks/make_equal_label_dataset.py
+++ dataset_module/notebooks/make_equal_label_dataset.py
@@ -6,6 +6,7 @@ import os
 log_dir = Path("dataset_module/downloads/log_files")
 pattern = re.compile(r"ERROR: \[youtube\]\s+([^\s:]+):")
 SEED = 42
+RATIO_TEST = 0.2
 
 
 results_ids = set()
@@ -32,24 +33,48 @@ for i in range(1, 21):
 
 df = pd.read_csv(csv_file, header = 0)
 df = df[~df[file_column].isin(results_ids)].reset_index(drop=True)
+# TODO : Missing files (error but video exist)
 
 
-print(df.shape, test) # TODO : Missing files (error but video exist)
+
+# AT THIS PART OUR DATASET IS CLEANED FROM ERRORS
 count = df['class'].value_counts()
 min_v = count.min()
-print(count)
-print(f"Minimum nb of samples for a label is :  {min_v} , gives us {min_v * count.shape[0]}")
+test_size = int(min_v * RATIO_TEST)
 
 balanced_df = (
     df
     .groupby('class', group_keys=False)               
-    .sample(n=min_v, random_state=SEED)
+    .sample(n=min_v, random_state=SEED)                           
+)
+
+train_idx = balanced_df.index
+balanced_df = balanced_df.reset_index(drop=True)
+
+
+remaining = df[~df.index.isin(train_idx)].reset_index(drop=True)
+test_df = (
+    remaining
+    .groupby('class', group_keys=False)               
+    .apply(lambda grp: grp.sample(
+        n=min(len(grp), test_size),
+        random_state=SEED
+    ))
     .reset_index(drop=True)                           
 )
 
+balanced_df['group_name'] = 'train'
+test_df['group_name'] = 'test'
+
 print(balanced_df.shape, balanced_df['class'].value_counts())
+print(test_df.shape, test_df['class'].value_counts())
 
-
+# Save balanced dataset
 output_path = 'dataset_module/data/processed_data/balanced_vggsound.csv'
 balanced_df.to_csv(output_path, index=False)
 print(f"Saved balanced dataset to: {output_path}")
+
+# Save test dataset
+output_path = 'dataset_module/data/processed_data/test_vggsound.csv'
+test_df.to_csv(output_path, index=False)
+print(f"Saved test dataset to: {output_path}")
